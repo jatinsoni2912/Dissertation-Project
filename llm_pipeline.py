d0d95a8d1a97754d743931b652b6884bdbe13145
js4008 <js4008@hw.ac.uk>
2026-06-15 23:22:52 +0100

    added fixes for invalid tags

--- llm_pipeline.py
+++ llm_pipeline.py
@@ -8,3 +8,16 @@ load_dotenv()
  
 BLOCKED_KEYWORDS = ['INSERT', 'UPDATE', 'DELETE', 'DROP', 'CREATE',
                     'ALTER', 'TRUNCATE', 'GRANT', 'REVOKE', 'EXECUTE']
+
+INVALID_TAG_FIXES = {
+    "landuse = 'park'":        "leisure = 'park'",
+    "landuse='park'":          "leisure = 'park'",
+    "amenity = 'post office'": "amenity = 'post_office'",
+    "amenity = 'dog_walking'": "leisure = 'park'",
+    "amenity = 'dog walking'": "leisure = 'park'",
+    "amenity = 'coffee_shop'": "amenity = 'cafe'",
+    "amenity = 'supermarket'": "shop = 'supermarket'",
+    "highway = 'bike'":        "highway = 'cycleway'",
+    "highway = 'bicycle'":     "highway = 'cycleway'",
+    "highway = 'walking'":     "highway = 'footway'",
+}
