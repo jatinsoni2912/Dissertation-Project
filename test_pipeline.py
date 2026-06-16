d066e816e1a8fc97ab03df6507dfafa054ef69ac
js4008 <js4008@hw.ac.uk>
2026-06-17 00:34:11 +0100

    added examples of test queries

--- test_pipeline.py
+++ test_pipeline.py
@@ -2,3 +2,12 @@ import time
 from llm_pipeline import generate_sql
 from mcp_pipeline import generate_sql_with_mcp
 from database import execute_query
+
+test_queries = [
+    "Where can I go cycling in Edinburgh?",
+    "Find parks near Ferry Road",
+    "How many post offices are in Edinburgh?",
+    "Where can I walk my dog near Leith?",
+    "Find cafes within 500 metres of Princes Street",
+    "Find restaurants in Stockbridge"
+]
