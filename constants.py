fc318c790eac98a82587f8318ee211aee442d926
js4008 <js4008@hw.ac.uk>
2026-06-17 15:30:29 +0100

    added blocked words and sports examples

---
+++ constants.py
@@ -0,0 +1,9 @@
+BLOCKED_KEYWORDS = [
+    'INSERT', 'UPDATE', 'DELETE', 'DROP', 'CREATE',
+    'ALTER', 'TRUNCATE', 'GRANT', 'REVOKE', 'EXECUTE',
+]
+
+SPORTS = {
+    'football', 'soccer', 'cricket', 'tennis', 'rugby',
+    'basketball', 'bowls', 'hockey', 'netball', 'volleyball', 'golf',
+}
