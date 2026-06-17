6934fb48dadb89f3104895031622179069b35503
js4008 <js4008@hw.ac.uk>
2026-06-18 00:38:05 +0100

    implemented the full run method

--- sql_fixer.py
+++ sql_fixer.py
@@ -16,3 +16,22 @@ class SqlFixer:
     def run(self):
         self.fix_geography_casts()
         self.fix_alias_prefix()
+
+        if not self.is_city_wide and self.lon is not None and self.lat is not None:
+            self.rebuild_from_boundary_join()
+            self.inject_missing_spatial_filter()
+
+        self.fix_intersects_proximity()
+        self.fix_sport_hstore()
+
+        if self.is_city_wide:
+            self.strip_city_wide_dwithin()
+
+        self.fix_landuse_tags()
+        self.fix_leisure_table()
+        self.fix_broken_join()
+
+        if 'COUNT(' not in self.sql.upper() and 'LIMIT' not in self.sql.upper():
+            self.sql = self.sql.rstrip(';') + ';'
+
+        return self.sql, self.fixes
