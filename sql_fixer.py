00ff2f2b72ef72c30f3ddcf49762c3e5b7f9a73d
js4008 <js4008@hw.ac.uk>
2026-06-18 00:36:44 +0100

    created sqlfixer class and implemented init method

--- sql_fixer.py
+++ sql_fixer.py
@@ -1,3 +1,11 @@
 import re
 from sql_extractor import extract_sql
 from constants import LANDUSE_CORRECTIONS, LEISURE_POLYGON_TAGS
+
+class SqlFixer:
+    def __init__(self, sql, lon=None, lat=None, is_city_wide=False):
+        self.sql = extract_sql(sql)
+        self.lon = lon
+        self.lat = lat
+        self.is_city_wide = is_city_wide
+        self.fixes = []
