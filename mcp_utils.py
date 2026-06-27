c5d00303d9d0f42487eff7c6c885145c9298413e
js4008 <js4008@hw.ac.uk>
2026-06-27 22:20:14 +0100

    added radius values for different points

---
+++ mcp_utils.py
@@ -0,0 +1,9 @@
+BASE_RADIUS = {
+    'point_dense':   500,   
+    'point_medium':  750,   
+    'point_large':  1000,   
+    'tourism':      1500,   
+    'leisure':      1500,   
+    'sport':        3000,   
+    'default':      1000,   
+}
