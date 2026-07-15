d7c3a1c3c45498e8be8007f04df70316648ca968
js4008 <js4008@hw.ac.uk>
2026-07-15 02:01:23 +0100

    added other imports

--- evaluation.py
+++ evaluation.py
@@ -11,3 +11,6 @@ from datetime import datetime
 from evaluation_queries import (ALL_QUERIES, ALL_EXTENDED_QUERIES, EXPLICIT_DISTANCE_QUERIES, LANDMARK_PROXIMITY_QUERIES, SPORT_DEPRIVATION_QUERIES)
 from database import execute_query
 from llm_client import (PROVIDER_OLLAMA, PROVIDER_BEDROCK,  DEFAULT_OLLAMA_MODEL, DEFAULT_BEDROCK_MODEL)
+from pure_llm_pipeline import generate_sql_pure_llm, generate_sql_pure_llm_using_bedrock
+from llm_pipeline import generate_sql
+from mcp_pipeline import generate_sql_with_mcp
